b466d3c15468785dfad2d25c9fb4f1bd71f5fdbb
andy0926 <tanandy2011@hotmail.com>
2020-06-23 11:48:00 +0800

    First Commit

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+    State state;
+
+    void Start()
+    {
+        state=StartingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+    }
+} 
---
+++ Assets/State.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(14, 10)] [SerializeField] string storyText;
+    // Start is called before the first frame update
+    [SerializeField] State[] nextStates;
+    //[SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    public State[] GetNextStates(){
+        return nextStates;
+    }
+}
